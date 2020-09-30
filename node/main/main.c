9b453cdb8ff7d7eadda7cdbba1969b7e12906ad1
Ivan Kouzmine <ivan.kouz333@gmail.com>
2020-09-30 03:20:30 -0400

    working SPI

--- node/main/main.c
+++ node/main/main.c
@@ -9,6 +9,7 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "driver/spi_master.h"
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
@@ -18,6 +19,28 @@
 #define WIFI_PASS "slowestofbois"    
 #define MAXIMUM_RETRY  4
 #define PORT 25565
+
+#define PIN_NUM_MISO 19
+#define PIN_NUM_MOSI 18
+#define PIN_NUM_CLK 5
+#define PIN_NUM_CS 21
+#define DMA_CHAN 2
+
+typedef struct {
+    uint8_t cmd[8];
+    uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
+} radio_cmd_t;
+
+DRAM_ATTR static const radio_cmd_t cmds[]={
+    {{0xE8, 0x11, 0x80}, 3},
+    {{0xE8, 0x12, 0x0A}, 3},
+    {{0xE8, 0x12, 0x0F}, 3},
+    {{0xAA, 0x01}, 2},
+    {{0xAA, 0x00}, 2},
+    {{0x6A, 0x04}, 2} //Read temperature
+};
+
+spi_device_handle_t spi;
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -31,6 +54,55 @@ static EventGroupHandle_t s_wifi_event_group;
 static const char *TAG = "Node";
 
 static int s_retry_num = 0;
+
+void reset_dwm(){
+    gpio_pad_select_gpio(32);
+    gpio_set_direction(32, GPIO_MODE_OUTPUT_OD);
+    gpio_set_level(32, 0);
+
+    usleep(1);
+
+    gpio_pulldown_dis(32);
+    gpio_set_level(32, 1);
+
+    sleep(2);
+}
+
+void write_off(uint8_t addr, uint8_t off, uint8_t val){
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));
+
+    uint8_t cmd[3] = {(addr | 0xC0), off, val};
+    t.length=24;
+    t.tx_buffer=cmd;
+
+    spi_device_polling_transmit(spi, &t);
+}
+
+void initialize_dwm(){
+    write_off(0x24, 0x00, 0x04);
+}
+
+void radio_tx(spi_device_handle_t spi, const uint8_t *cmd, int len){
+    spi_transaction_t t;
+    if (len==0) return;             //no need to send anything
+    memset(&t, 0, sizeof(t));       //Zero out the transaction
+    t.length=len*8;                 //Len is in bytes, transaction length is in bits.
+    t.tx_buffer=cmd;               //Data
+    t.flags = SPI_TRANS_USE_RXDATA;
+    spi_device_polling_transmit(spi, &t);  //Transmit!
+
+    if(cmd[0] == 0x6A){
+        int temp = *(uint32_t*)t.rx_data;
+        ESP_LOGI(TAG, "UWB Temperature is %d", temp);
+    }
+}
+
+void radio_get_temperature(spi_device_handle_t spi){
+    for(int i = 0; i < 6; i++){
+        radio_tx(spi, cmds[i].cmd, cmds[i].databytes&0x1F);
+    }
+}
 
 static void do_retransmit(const int sock)
 {
@@ -64,6 +136,11 @@ static void do_retransmit(const int sock)
                 }
                 
             }
+
+            if(!strcmp(rx_buffer, "temp")){
+                radio_get_temperature(spi);
+            }
+            
 
             // send() can return less bytes than supplied length.
             // Walk-around for robust implementation. 
@@ -218,6 +295,13 @@ void wifi_init_sta(void)
     vEventGroupDelete(s_wifi_event_group);
 }
 
+void radio_spi_pre_transfer_callback(spi_transaction_t *t)
+{
+    //int dc=(int)t->user;
+    //gpio_set_level(PIN_NUM_DC, dc);
+    ESP_LOGI(TAG, "Transfer on SPI");
+}
+
 void app_main(void)
 {
     //Initialize NVS
@@ -236,6 +320,31 @@ void app_main(void)
 
     gpio_pad_select_gpio(14);
     gpio_set_direction(14, GPIO_MODE_OUTPUT);
+
+    spi_bus_config_t buscfg={
+        .miso_io_num = PIN_NUM_MISO,
+        .mosi_io_num = PIN_NUM_MOSI,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 0
+    };
+
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=10*1000,           //Clock out at 10 MHz
+        .mode=0,                                //SPI mode 0
+        .spics_io_num = PIN_NUM_CS,               //CS pin
+        .queue_size = 7,                          //We want to be able to queue 7 transactions at a time
+        .pre_cb=radio_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+    };
+
+    spi_bus_initialize(1, &buscfg, DMA_CHAN);
+
+    spi_bus_add_device(1, &devcfg, &spi);
+
+    reset_dwm();
+
+    initialize_dwm();
 
     xTaskCreate(tcp_server_task, "tcp_server", 4096, NULL, 5, NULL);
 }
--- test_scripts/main.py
+++ test_scripts/main.py
@@ -5,7 +5,7 @@ import time
 DEVICE_COUNT = 3 #Not used atm
 NET_BASE = '192.168.1.' #Change depending on network
 RL = 2 #From IP.2
-RH = 20 #To IP.20
+RH = 8 #To IP.20
 
 class Command:
     def __init__(self, data):
