4d3ece6d831d70e404d3dba8076ec1e9383b911a
Tanner Andrulis <tandruli@purdue.edu>
2020-12-03 13:29:11 -0500

    Node cleaning added.

--- Master.py
+++ Master.py
@@ -3,9 +3,6 @@ import Drawer_CV2
 import SLAM
 
 FILES = ['ivan_room.txt']
-EMERGENCY = ['samapt/data1.txt', 'samapt/data4.txt', 'samapt/data7.txt']
-
-EMERGENCY_SAFE = False
 
 def from_files(files=None):
 	if not files:
@@ -28,30 +25,19 @@ def clean(nodes):
 	return nodes
 
 def run(dicts):
-	try:
-		nodes = []
-		for k, v in dicts.items():
-			n = Node.Node(0, 0, 0, k)
-			n.from_list(v)
-			n.segment()
-			nodes.append(n)
+	nodes = []
+	for k, v in dicts.items():
+		n = Node.Node(0, 0, 0, k)
+		n.from_list(v)
+		n.segment()
+		nodes.append(n)
 
-		if EMERGENCY_SAFE:
-			nodes = clean(nodes)
-			if len(nodes) < 3:
-				emergencies = from_files(EMERGENCY)
-				while len(nodes) < 3:
-					nodes.append(SLAM.get_worst(emergencies, nodes))
+	nodes = clean(nodes)
 
-		s = SLAM.SLAM(nodes)
-		distances = s.solve_multi()
-		Drawer_CV2.draw_to_image(nodes, [], 'app/static/img/render.png')
-		return distances
-	except Exception as e:
-		if EMERGENCY_SAFE:
-			return [(2, 1, 2.58373623847102), (2, 0, 4.636262075138433), (1, 0, 2.2867623686266314)]
-		else:
-			raise e
+	s = SLAM.SLAM(nodes)
+	distances = s.solve_multi()
+	Drawer_CV2.draw_to_image(nodes, [], 'app/static/img/render.png')
+	return distances
 
 if __name__ == '__main__':
 	print(run({}))
